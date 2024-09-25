5857d534ef6fb12f6c0878387e080b0d07246c82
Platon <xnpltn@gmail.com>
2024-09-26 00:21:13 +0200

    results in main

--- src/main.rs
+++ src/main.rs
@@ -16,31 +16,18 @@ struct Args {
     skip_prompt: Option<bool>,
 }
 
-fn main() {
-    let licenses = license::Licenses::fetch_licenses();
-
-    match licenses {
-        Ok(licenses) => {
-            let valid_license_names = &licenses.get_license_keys();
-
-            let args: Args = Args::parse();
-            let license_name = &args.license_name.unwrap_or(String::from("")).to_lowercase();
-            let license_content: LicenseContent;
-
-            if valid_license_names.contains(&license_name) {
-                license_content = licenses.get_license_from_key(&license_name);
-            } else {
-                let license = helpers::select(&licenses.get_license_names());
-                license_content = licenses.get_license_from_name(&license);
-            }
-
-            helpers::fill_content(&license_content, args.skip_prompt.unwrap_or(false));
-        }
-        Err(err) => {
-            yellow_ln_bold!("\nErro Occured while getting Licenses.\n");
-            red_ln_bold!("{}\n", err);
-            green_ln_bold!("Tip: Check Your Internet Connection And try again\n");
-            process::exit(1);
-        }
+fn main() -> Result<(), String> {
+    let licenses = license::Licenses::fetch_licenses()?;
+    let valid_license_names = &licenses.get_license_keys();
+    let args: Args = Args::parse();
+    let license_name = &args.license_name.unwrap_or(String::from("")).to_lowercase();
+    let license_content: LicenseContent;
+    if valid_license_names.contains(&license_name) {
+        license_content = licenses.get_license_from_key(&license_name);
+    } else {
+        let license = helpers::select(&licenses.get_license_names());
+        license_content = licenses.get_license_from_name(&license);
     }
+    helpers::fill_content(&license_content, args.skip_prompt.unwrap_or(false));
+    Ok(())
 }
